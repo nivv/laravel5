6bf7fca144bef97eebd51c8f71bd582abe6c6947
Albin-Github <albin.nilsson89@gmail.com>
2015-01-10 14:29:34 +0100

    added live templates

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -17,18 +17,45 @@
                 </div>
             @endif
             {!! Form::open(['route' => 'pages.store']) !!}
-            <div class="form-group">
-                {!! Form::label('title','Title') !!}
-                {!! Form::text('title', '', array('class' => 'form-control')) !!}
-            </div>
-            <div class="form-group">
-                {!! Form::label('body','Body') !!}
-                {!! Form::textarea('body', '', array('class' => 'form-control')) !!}
-            </div>
-            <div class="form-group">
-                {!! Form::submit('Create', array('class' => 'btn btn-success')) !!}
-            </div>
-
+                <!-- Title Form Input -->
+                <div class="form-group">
+                    {!! Form::label('title','Title:') !!}
+                    {!! Form::text('title', null, array('class' => 'form-control', 'placeholder' => 'Title')) !!}
+                </div>
+                <!-- Password Form Input -->
+                <div class="form-group">
+                    {!! Form::label('password','Password:') !!}
+                    {!! Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) !!}
+                </div>
+                
+                <!-- Confirm Password Form Input -->
+                <div class="form-group">
+                    {!! Form::label('password_confirm','Confirm Password:') !!}
+                    {!! Form::password('password_confirm', array('class' => 'form-control', 'placeholder' => 'Confirm Password')) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('body','Body') !!}
+                    {!! Form::textarea('body', '', array('class' => 'form-control')) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::submit('Create', array('class' => 'btn btn-success')) !!}
+                </div>
+                <h4>To remember</h4>
+                <pre>
+                     &lt;!-- $VALUE$ Form Input --&gt;
+&lt;div class=&quot;form-group&quot;&gt;
+                    {|| Form::label(&apos;$NAME$&apos;,&apos;$VALUE$:&apos;) ||}
+                    {|| Form::text(&apos;$NAME$&apos;, null, array(&apos;class&apos; =&gt; &apos;form-control&apos;, &apos;placeholder&apos; =&gt; &apos;$VALUE$&apos;)) ||}
+                    &lt;/div&gt;
+                </pre>
+                                <pre>
+                    &#x3C;!-- $VALUE$ Form Input --&#x3E;
+&#x3C;div class=&#x22;form-group&#x22;&#x3E;
+                    {// Form::label(&#x27;$NAME$&#x27;,&#x27;$VALUE$:&#x27;) //}
+                    {// Form::password(&#x27;$NAME$&#x27;, null, array(&#x27;class&#x27; =&#x3E; &#x27;form-control&#x27;, &#x27;placeholder&#x27; =&#x3E; &#x27;$VALUE$&#x27;)) //}
+                    &#x3C;/div&#x3E;
+                </pre>
             {!! Form::close() !!}
         </div>
     </div>
